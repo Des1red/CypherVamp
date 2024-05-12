19b3e8699c90cf60f057b3a40155db2725a8a516
Des1red <justbeasteasy@gmail.com>
2024-05-12 16:24:04 +0000

    last update 16:23 12/5/24

--- installer.sh
+++ installer.sh
@@ -18,6 +18,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Creating necessary files..."
+# Assuming Scanners.go is located in the same directory as this script
 go build Scanners.go
 
 # Check if build was successful
@@ -28,9 +29,22 @@ fi
 echo ""
 echo ""
 echo "Installation completed successfully"
-echo "To use the Cypher tool, please follow these steps:"
-echo "1. Navigate to the directory containing the 'Scanners' executable."
-echo "2. Add an alias to the 'Scanners' executable by running:"
-echo "   For Bash: alias cypher=\"$(pwd)/Scanners\""
-echo "   For Zsh:  alias cypher=\"$(pwd)/Scanners\""
+
+# Define the alias command
+alias cypher="/home/kali/Scanners"
+
+# Save the changes to the Bash configuration file of the current user
+echo 'alias cypher="$(pwd)/Scanners"' >> ~/.bashrc
+
+# Check if Zsh is installed
+if [ -n "$(command -v zsh)" ]; then
+    # Save the changes to the Zsh configuration file of the current user
+   echo 'alias cypher="$(pwd)/Scanners"' >> ~/.zshrc
+fi
+
+# Apply the changes to the current shell session
+source ~/.bashrc
+source ~/.zshrc  
+
+echo "Alias 'cypher' has been set to '\"$(pwd)/Scanners\'."
 echo "3. You can now use the 'cypher' command to start the scan."
